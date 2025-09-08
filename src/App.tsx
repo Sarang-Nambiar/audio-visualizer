8c3747974a471badedc3afc1fde3a31d7d1cea90
Sarang Nambiar <sarangnambiar455@gmail.com>
2025-09-09 00:30:20 +0800

    feat: Sample box rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Canvas } from '@react-three/fiber';
 import './App.css';
 import gh_icon from './assets/github-icon.png';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
 
@@ -11,14 +13,24 @@ function App() {
     <>
       <div className='navbar'>
         <a href='/'>
-          <span className='nav-title'>Audio Visualizer</span>
+          <span className='nav-title'>DisShazam</span>
         </a>
         <a href='https://github.com/Sarang-Nambiar/audio-visualizer'>
           <img src={gh_icon} alt='gh-logo' width={50} height={50} />
         </a>
       </div>
-      <div className='container'>
+      <div className='canvas'>
+        <Canvas camera={{position:[10, 10, 10], near:5, far: 50, fov: 50}}>
+          <mesh>
+              <boxGeometry args={[6, 6, 6]}/>
+              <meshStandardMaterial />
+              <ambientLight intensity={0.2} />
+              <directionalLight color="white" position={[0, 2, 5]} />
+          </mesh>
+          <OrbitControls enableDamping={true} enableZoom={false}/>
+        </Canvas>
       </div>
+      <p className='label'>Spin the box!</p>
     </>
   )
 }
